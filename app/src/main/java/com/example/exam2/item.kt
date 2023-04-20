55c97eaea82bb0e9174f8e5149840f163ce2def7
Binhdeptrai1 <ngovanbinh2103@gmail.com>
2023-04-20 14:41:34 +0700

    Lab

---
+++ app/src/main/java/com/example/exam2/item.kt
@@ -0,0 +1,8 @@
+package com.example.exam2
+
+data class itemDs (val TenSP:String?=null,
+                   val LoaiSP:String?=null,
+                   val GiaSP:String?=null,
+                   val Img:String?=""
+
+)
